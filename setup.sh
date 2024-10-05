a08ac74797d912bdfef5262588ca0854d577c582
iced-penguin <icedpenguin0504@gmail.com>
2024-10-05 20:04:09 +0900

    chore: セットアップ方法を変更
    コピーするのではなくシンボリックリンクを作成ように変更
    コマンドの置き場所を~/binから/usr/local/binに変更

--- setup.sh
+++ setup.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
-# ~/binに全てのスクリプトを配置する
 
-if [ ! -d ~/bin ]; then
-  mkdir ~/bin
-fi
-
-if [[ ":$PATH:" != *":$HOME/bin:"* ]]; then
-  echo 'export PATH="$HOME/bin:$PATH"' >> ~/.zshrc
-  source ~/.zshrc
-fi
-
+# /usr/local/bin にシンボリックリンクを貼る
 ls | grep -v -e README.md -e setup.sh | while read line; do
   chmod 744 "$line"
-  cp "$line" ~/bin
+  ln -s "$(pwd)/$line" /usr/local/bin
 done
